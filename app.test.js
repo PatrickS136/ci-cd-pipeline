70479c89e6566351b241537a6cad1c4fe017c09c
Patrick Sumarli <tricksumarli@gmail.com>
2022-10-01 19:10:51 +0930

    Initialise Express app

---
+++ app.test.js
@@ -0,0 +1,19 @@
+const app = require("./app")
+const supertest = require("supertest")
+const request = supertest(app)
+
+describe("/test succeed", () => {
+    it("should return halo dunia", async () => {
+        const response = await request.get("/test")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Halo dunia");
+    })
+})
+
+describe("/test fail", () => {
+    it("should return hello world", async () => {
+        const response = await request.get("/test")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Hello world");
+    })
+})
